5d54b2510eb1c1d7e98c5b3f36d87b4d6caa4150
Nathan Karsten <nwkarste@umich.edu>
2022-08-30 15:20:26 -0400

    Update and rename bash_profile.sh to bash_aliases.sh

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -1,6 +1,6 @@
 myalias() {
     lastcommand=$(history 2 | head -n 1 | sed 's/^[ ]*[0-9]*[ ]*//')
-    cat << EOF_BASH_PROFILE >>$HOME/.bash_profile
+    cat << EOF_BASH_PROFILE >>$HOME/.bash_aliases
 
 # myalias ${1}
 alias $1="${lastcommand}"
