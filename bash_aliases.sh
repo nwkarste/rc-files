42a5168672089a2e724e499df9c6774ae3c80c30
Nathan Karsten <nwkarste@umich.edu>
2022-08-30 15:21:12 -0400

    Update bash_aliases.sh

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -1,8 +1,8 @@
-myalias() {
+svalias() {
     lastcommand=$(history 2 | head -n 1 | sed 's/^[ ]*[0-9]*[ ]*//')
     cat << EOF_BASH_PROFILE >>$HOME/.bash_aliases
 
-# myalias ${1}
+# svalias ${1}
 alias $1="${lastcommand}"
 EOF_BASH_PROFILE
 
